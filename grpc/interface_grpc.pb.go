5ce9cb593aaa4a2599a51b7f5a233af29ca1c3bc
ChrBank <chbl@itu.dk>
2022-11-10 13:13:31 +0100

    implement Service and Message methods

--- grpc/interface_grpc.pb.go
+++ grpc/interface_grpc.pb.go
@@ -22,7 +22,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type RequestServiceClient interface {
-	Request(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Request, error)
+	Request(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error)
+	Join(ctx context.Context, in *Join, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type requestServiceClient struct {
@@ -33,9 +34,18 @@ func NewRequestServiceClient(cc grpc.ClientConnInterface) RequestServiceClient {
 	return &requestServiceClient{cc}
 }
 
-func (c *requestServiceClient) Request(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Request, error) {
-	out := new(Request)
+func (c *requestServiceClient) Request(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error) {
+	out := new(Reply)
 	err := c.cc.Invoke(ctx, "/request.RequestService/request", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *requestServiceClient) Join(ctx context.Context, in *Join, opts ...grpc.CallOption) (*Empty, error) {
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, "/request.RequestService/join", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +56,8 @@ func (c *requestServiceClient) Request(ctx context.Context, in *Request, opts ..
 // All implementations must embed UnimplementedRequestServiceServer
 // for forward compatibility
 type RequestServiceServer interface {
-	Request(context.Context, *Request) (*Request, error)
+	Request(context.Context, *Request) (*Reply, error)
+	Join(context.Context, *Join) (*Empty, error)
 	mustEmbedUnimplementedRequestServiceServer()
 }
 
@@ -54,8 +65,11 @@ type RequestServiceServer interface {
 type UnimplementedRequestServiceServer struct {
 }
 
-func (UnimplementedRequestServiceServer) Request(context.Context, *Request) (*Request, error) {
+func (UnimplementedRequestServiceServer) Request(context.Context, *Request) (*Reply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Request not implemented")
+}
+func (UnimplementedRequestServiceServer) Join(context.Context, *Join) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Join not implemented")
 }
 func (UnimplementedRequestServiceServer) mustEmbedUnimplementedRequestServiceServer() {}
 
@@ -88,6 +102,24 @@ func _RequestService_Request_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _RequestService_Join_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Join)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RequestServiceServer).Join(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/request.RequestService/join",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RequestServiceServer).Join(ctx, req.(*Join))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // RequestService_ServiceDesc is the grpc.ServiceDesc for RequestService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -99,91 +131,9 @@ var RequestService_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "request",
 			Handler:    _RequestService_Request_Handler,
 		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "grpc/interface.proto",
-}
-
-// JoinServiceClient is the client API for JoinService service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type JoinServiceClient interface {
-	Join(ctx context.Context, in *Join, opts ...grpc.CallOption) (*Join, error)
-}
-
-type joinServiceClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewJoinServiceClient(cc grpc.ClientConnInterface) JoinServiceClient {
-	return &joinServiceClient{cc}
-}
-
-func (c *joinServiceClient) Join(ctx context.Context, in *Join, opts ...grpc.CallOption) (*Join, error) {
-	out := new(Join)
-	err := c.cc.Invoke(ctx, "/request.JoinService/join", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// JoinServiceServer is the server API for JoinService service.
-// All implementations must embed UnimplementedJoinServiceServer
-// for forward compatibility
-type JoinServiceServer interface {
-	Join(context.Context, *Join) (*Join, error)
-	mustEmbedUnimplementedJoinServiceServer()
-}
-
-// UnimplementedJoinServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedJoinServiceServer struct {
-}
-
-func (UnimplementedJoinServiceServer) Join(context.Context, *Join) (*Join, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Join not implemented")
-}
-func (UnimplementedJoinServiceServer) mustEmbedUnimplementedJoinServiceServer() {}
-
-// UnsafeJoinServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to JoinServiceServer will
-// result in compilation errors.
-type UnsafeJoinServiceServer interface {
-	mustEmbedUnimplementedJoinServiceServer()
-}
-
-func RegisterJoinServiceServer(s grpc.ServiceRegistrar, srv JoinServiceServer) {
-	s.RegisterService(&JoinService_ServiceDesc, srv)
-}
-
-func _JoinService_Join_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Join)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(JoinServiceServer).Join(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/request.JoinService/join",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(JoinServiceServer).Join(ctx, req.(*Join))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-// JoinService_ServiceDesc is the grpc.ServiceDesc for JoinService service.
-// It's only intended for direct use with grpc.RegisterService,
-// and not to be introspected or modified (even as a copy)
-var JoinService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "request.JoinService",
-	HandlerType: (*JoinServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "join",
-			Handler:    _JoinService_Join_Handler,
+			Handler:    _RequestService_Join_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- main.go
+++ main.go
@@ -2,22 +2,131 @@ package main
 
 import (
 	request "Lucasfth/go-ass4/grpc/grpc"
+	"bufio"
 	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	"google.golang.org/grpc"
 )
-
 func main() {
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
+	ownPort := int32(arg1) + 5000
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	p := &peer{
+		id:            ownPort,
+		requestAmount: 0,
+		peers:         make(map[int32]request.RequestServiceClient),
+		response:      make(map[int32]int32),
+		ctx:           ctx,
+	}
+
+	// Create listener tcp on port ownPort
+	list, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+	if err != nil {
+		log.Fatalf("Failed to listen on port: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	request.RegisterRequestServiceServer(grpcServer, p)
+
+	go func() {
+		if err := grpcServer.Serve(list); err != nil {
+			log.Fatalf("failed to server %v", err)
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		port := int32(5000) + int32(i)
+
+		if port == ownPort {
+			continue
+		}
+
+		var conn *grpc.ClientConn
+		log.Printf("Trying to dial: %v\n", port)
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure(), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+		defer conn.Close()
+		c := request.NewRequestServiceClient(conn)
+		p.peers[port] = c
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		p.sendRequestToAll()
+		numberOfPeers := len(p.response)
+
+		for i := 0; i < numberOfPeers; i++ {
+			if p.response[int32(i)] == 0 {
+				break
+			}
+			if numberOfPeers == i {
+				if p.decideControl() {
+					p.criticalSection()
+				}
+				p.clearResonse()
+			}
+		}
+	}
 }
 
 type peer struct {
 	request.UnimplementedRequestServiceServer
-	id              int32
-	amountOfRequest int32
-	peers           map[int32]request.RequestServiceClient
-	ctx             context.Context
+	id            int32
+	requestAmount int32
+	peers         map[int32]request.RequestServiceClient
+	response      map[int32]int32
+	ctx           context.Context
 }
 
-func (p *peer) request(ctx context.Context, req *request.Request) (*request.Request, error) {
-	id := req.Id
-	amount := req.RequestAmount
+func (p *peer) Request(ctx context.Context, req *request.Request) (*request.Reply, error) {
+	p.requestAmount++
+
+	rep := &request.Reply{Id: p.id, RequestAmount: p.requestAmount}
+	return rep, nil
+}
+
+func (p *peer) sendRequestToAll() {
+	request := &request.Request{Id: p.id, RequestAmount: p.requestAmount}
+	for _, peer := range p.peers {
+		reply, err := peer.Request(p.ctx, request)
+		if err != nil {
+			log.Fatalf("Could not send request: %v", err)
+		}
+		log.Printf("Got reply from id %v: %v\n", p.id, reply.RequestAmount)
+		p.response[reply.Id] = reply.RequestAmount
+	}
+}
+
+func (p *peer) criticalSection() {
+	log.Printf("%v is now pilot", p.id)
+}
+
+func (p *peer) decideControl() bool {
+	numberOfPeers := len(p.response)
+	for i := 0; i < numberOfPeers; i++ {
+		if p.response[int32(i)] > p.requestAmount {
+			return false
+		} else if p.response[int32(i+1)] == p.requestAmount {
+			if p.id < int32(i+1) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (p *peer) clearResonse() {
+	numberOfPeers := len(p.response)
+	for i := 0; i < numberOfPeers; i++ {
+		p.response[int32(i)] = 0
+	}
 }
